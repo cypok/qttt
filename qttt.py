6ee35f84ce27ef2bf7c976cba9069e1a482e1360
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2009-12-09 01:40:51 +0600

    Загрузка настроек

--- qttt.py
+++ qttt.py
@@ -3,8 +3,14 @@ import sys
 from PyQt4 import QtCore, QtGui
 
 from main_ui import Ui_MainWindow
+import yaml
+import os
 
 class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
+    def load_settings(self):
+        with open(os.environ['HOME']+'/.tttrc') as f:
+            self.settings = yaml.load(f.read())
+
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
 
@@ -13,10 +19,13 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
 
         self.gb_current.hide()
 
-#        self.resize(250, 150)
-#        self.setWindowTitle('statusbar')
+        self.move(400, 100)
+        self.resize(350, 550)
 
-#        self.statusBar().showMessage('Ready')
+        self.load_settings()
+
+        self.setWindowTitle("QTTT - %s" % self.settings['base_url'])
+
 
 app = QtGui.QApplication(sys.argv)
 main = MainWindow()
