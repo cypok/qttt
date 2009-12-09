295d6608714a46965e4916eea56e346b3bd4be3e
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2009-12-09 12:54:43 +0600

    Простая отправка апдейта

--- qttt.py
+++ qttt.py
@@ -1,18 +1,44 @@
 #!/usr/bin/env python
+# -*- coding: utf8 -*-
+
 import sys
 from PyQt4 import QtCore, QtGui
 
 from main_ui import Ui_MainWindow
-import yaml
 import os
+import yaml
+import json
+import httplib2
+import urllib
+
+class Remote:
+    def __init__(self):
+        with open(os.environ['HOME']+'/.tttrc') as f:
+            self.config = yaml.load(f.read())
+        self.http = httplib2.Http()
+
+    def getConfig(self, name):
+        return self.config[name]
+
+    def sendUpdate(self, text):
+        url = "%s/updates.json?%s" % (self.getConfig("base_url"), urllib.urlencode({'api_key':self.getConfig('api_key')}))
+        type = "POST"
+        data = "update[human_message]=%s" % text
+        res = json.loads(self.http.request(url, type, data)[1])
+        print res
+        return True
 
 class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
-    def load_settings(self):
-        with open(os.environ['HOME']+'/.tttrc') as f:
-            self.settings = yaml.load(f.read())
+    def messageBox(self, title, text):
+        box = QtGui.QMessageBox()
+        box.setWindowTitle(title)
+        box.setText(text)
+        return box.exec_()
 
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
+
+        self.remote = Remote()
 
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.setupUi(self)
@@ -22,9 +48,23 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         self.move(400, 100)
         self.resize(350, 550)
 
-        self.load_settings()
+        self.setWindowTitle("QTTT - %s" % self.remote.getConfig('base_url'))
 
-        self.setWindowTitle("QTTT - %s" % self.settings['base_url'])
+        self.connect(self.pb_update, QtCore.SIGNAL("clicked()"), self.sendUpdate)
+        self.connect(self.le_update, QtCore.SIGNAL("returnPressed()"), self.sendUpdate)
+
+
+    def sendUpdate(self):
+        text = self.le_update.text()
+        if text.isEmpty():
+            return
+
+        if self.remote.sendUpdate(text):
+            self.messageBox(u"Отправка апдейта", u"Апдейт успешно отправлен")
+        else:
+            self.messageBox(u"Отправка апдейта", u"Произошла неизвестная и ужасная ошибка")
+
+
 
 
 app = QtGui.QApplication(sys.argv)
