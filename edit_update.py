e9be6dc170d894fb64baa7bb6af24a01f6a105be
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2010-04-06 11:54:59 +0700

    Выкинул функции конвертирования м/у datetime и QDateTime

--- edit_update.py
+++ edit_update.py
@@ -3,12 +3,6 @@ from edit_update_ui import Ui_Dialog
 from time import mktime
 from datetime import datetime
 
-def datetime2QDateTime(dt):
-    return QtCore.QDateTime.fromTime_t(int(mktime(dt.timetuple())))
-
-def QDateTime2datetime(qdt):
-    return datetime.fromtimestamp(qdt.toTime_t())
-
 class EditUpdate(QtGui.QDialog, Ui_Dialog):
     def __init__(self, update, parent = None):
         QtGui.QDialog.__init__(self, parent)
@@ -17,7 +11,7 @@ class EditUpdate(QtGui.QDialog, Ui_Dialog):
         self.hours.setSingleStep(0.25)
 
         self.message.setText(update.message)
-        self.started_at.setDateTime(datetime2QDateTime(update.started_at))
+        self.started_at.setDateTime(update.started_at)
 
         if update.finished_at is not None:
             self.finished_at.setDateTime(datetime2QDateTime(update.finished_at))
@@ -28,6 +22,6 @@ class EditUpdate(QtGui.QDialog, Ui_Dialog):
 
     def changed_update(self):
         return {"message" : self.message.text(),
-                "started_at" : QDateTime2datetime(self.started_at.dateTime()),
-                "finished_at" : QDateTime2datetime(self.finished_at.dateTime()) if self.finished_at.isEnabled() else None,
+                "started_at" : self.started_at.dateTime().toPyDateTime(),
+                "finished_at" : self.finished_at.dateTime().toPyDateTime() if self.finished_at.isEnabled() else None,
                 "hours" : self.hours.value() if self.hours.isEnabled() else None}
