1602bcc706e2575953d50d7864dc3b327d403dca
Ivan Novikov <nia.afti@gmail.com>
2012-12-05 17:20:38 +0400

    Показ времени в локальной таймзоне
    Время конвертируется в локальную таймзону перед показом в ленте и в
    диалоге редактирования.
    После редактирования оно конвертируется обратно перед сохранением.
    При этом сами объекты Update остаются нетронутыми, в них (а также в
    локальном SQL кэше) время хранится в таймзоне сервера.

--- edit_update.py
+++ edit_update.py
@@ -2,6 +2,7 @@ from PyQt4 import QtGui, QtCore
 from edit_update_ui import Ui_Dialog
 from time import mktime
 from datetime import datetime
+from update import Update
 
 class EditUpdate(QtGui.QDialog, Ui_Dialog):
     def __init__(self, update, parent = None):
@@ -11,10 +12,11 @@ class EditUpdate(QtGui.QDialog, Ui_Dialog):
         self.hours.setSingleStep(0.25)
 
         self.message.setText(update.message)
-        self.started_at.setDateTime(update.started_at)
+        self.started_at.setDateTime(Update.to_local_timezone(update.started_at))
+        self.tzinfo = update.started_at.tzinfo # store initial tzinfo to be able to convert back
 
         if update.finished_at is not None:
-            self.finished_at.setDateTime(update.finished_at)
+            self.finished_at.setDateTime(Update.to_local_timezone(update.finished_at))
             self.hours.setValue(update.hours)
         else:
             self.finished_at.setEnabled(False)
@@ -22,6 +24,6 @@ class EditUpdate(QtGui.QDialog, Ui_Dialog):
 
     def changed_update(self):
         return {"message" : self.message.text(),
-                "started_at" : self.started_at.dateTime().toPyDateTime(),
-                "finished_at" : self.finished_at.dateTime().toPyDateTime() if self.finished_at.isEnabled() else None,
+                "started_at" : Update.from_local_timezone(self.started_at.dateTime().toPyDateTime(), self.tzinfo),
+                "finished_at" : Update.from_local_timezone(self.finished_at.dateTime().toPyDateTime(), self.tzinfo) if self.finished_at.isEnabled() else None,
                 "hours" : self.hours.value() if self.hours.isEnabled() else None}
